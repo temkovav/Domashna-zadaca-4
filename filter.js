1fd0c6395d38bd3d83196dca105e83ee8ff4dcd8
temkovav <116488055+temkovav@users.noreply.github.com>
2023-01-07 21:20:16 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     $("#search").on("keyup", function()
     {
         var value = $(this).val().toLowerCase();
-        $("#nestedList li").filter(function()
+        $("#listi li").filter(function()
         {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
